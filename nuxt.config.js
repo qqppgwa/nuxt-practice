356283735c21b90f467a3a392e50ddda133c6771
gwa <qqppgwa@gmail.com>
2019-07-24 15:42:20 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,30 +5,28 @@ export default {
    */
     head: {
         title: process.env.npm_package_name || '',
-        meta: [
-            {
-                charset: 'utf-8'
-            },
-            {
-                name: 'viewport',
-                content: 'width=device-width, initial-scale=1'
-            },
-            {
-                hid: 'description',
-                name: 'description',
-                content: process.env.npm_package_description || ''
-            }
+        meta: [{
+            charset: 'utf-8'
+        },
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+        },
+        {
+            hid: 'description',
+            name: 'description',
+            content: process.env.npm_package_description || ''
+        }
         ],
-        link: [
-            {
-                rel: 'icon',
-                type: 'image/x-icon',
-                href: '/favicon.ico'
-            },
-            {
-                rel: 'stylesheet',
-                href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
-            }
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
+        }
         ]
     },
     /*
@@ -60,6 +58,6 @@ export default {
         extend (config, ctx) {}
     },
     router: {
-        base: '/f2eChallenge/'
+        base: '/dist/f2eChallenge/'
     }
 };
