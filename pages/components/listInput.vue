472aca7d8d2839ba14452e41c8732142b6983704
gwa <qqppgwa@gmail.com>
2019-07-24 18:03:08 +0800

    Revert "no message"
    This reverts commit 23c1877f7261cee1a90aa741f0bfc2d80442c867.

---
+++ pages/components/listInput.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="inputContainer">
+        <input type="text" placeholder="add a new mission…" v-model="input">
+        <button @click="addTodo"></button>
+    </div>
+</template>
+<script>
+
+export default {
+    data () {
+        return {
+            input: ''
+            // list: {
+            //     input: '',
+            //     done: false
+            // }
+        };
+    },
+    methods: {
+        addTodo () {
+            console.log(this.input);
+            this.$emit('addTodo', { input: this.input,
+                done: false });
+            setTimeout(() => {
+                this.input = '';
+            }, 100);
+        }
+    }
+};
+</script>
+<style scoped>
+.inputContainer {
+    width: 445px;
+    height: 56px;
+    background-color: #fff;
+    padding: 16px;
+    display: flex;
+    font-family: Roboto;
+}
+input {
+    height: 100%;
+    background-color: #fff;
+    border: none;
+    width: 100%;
+    outline: none;
+    font-family: Roboto;
+    font-weight: 900;
+}
+input[placeholder] {
+    color: #ff4384;
+}
+input::placeholder {
+    color: #ff4384;
+    font-style: italic;
+    font-size: 16px;
+    font-weight: 900;
+    font-family: Roboto;
+}
+button {
+    background-color: #fff;
+    border: none;
+    width: 24px;
+    height: 24px;
+    background-position: center;
+    background-size: cover;
+    background-image: url(/f2eChallenge/images/add.svg);
+    outline: none;
+}
+</style>
