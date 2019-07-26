5caefe8b30a817a3af1a1e2d1bcc3b92eed49c99
gwa <qqppgwa@gmail.com>
2019-07-27 01:39:52 +0800

    no message

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,63 @@
 export default {
-  mode: 'spa',
-  /*
+    mode: 'spa',
+    /*
    ** Headers of the page
    */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [{
-        charset: 'utf-8'
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
-      },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || ''
-      }
-    ],
-    link: [{
-        rel: 'icon',
-        type: 'image/x-icon',
-        href: '/favicon.ico'
-      },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
-      }
-    ]
-  },
-  /*
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [{
+            charset: 'utf-8'
+        },
+        {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1'
+        },
+        {
+            hid: 'description',
+            name: 'description',
+            content: process.env.npm_package_description || ''
+        }
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css?family=Roboto&display=swap'
+        }
+        ]
+    },
+    /*
    ** Customize the progress-bar color
    */
-  loading: {
-    color: '#fff'
-  },
-  /*
+    loading: {
+        color: '#fff'
+    },
+    /*
    ** Global CSS
    */
-  css: ['element-ui/lib/theme-chalk/index.css'],
-  /*
+    css: ['element-ui/lib/theme-chalk/index.css'],
+    /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['@/plugins/element-ui'],
-  /*
+    plugins: ['@/plugins/element-ui'],
+    /*
    ** Nuxt.js modules
    */
-  modules: [],
-  /*
+    modules: [],
+    /*
    ** Build configuration
    */
-  build: {
-    transpile: [/^element-ui/],
-    /*
+    build: {
+        transpile: [/^element-ui/],
+        /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
-  },
-  router: {
-    base: '/f2eChallenge/'
-  }
+        extend (config, ctx) {}
+    },
+    router: {
+        base: '/f2eChallange/'
+    }
 };
